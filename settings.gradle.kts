64f4e64c364e134f1230cb0c6d4a9eae58bae10e
Jonathan Leitschuh <Jonathan.Leitschuh@gradle.com>
2022-01-15 00:27:33 -0500

    Fix gradle enterprise convention plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 }
 plugins {
     id("com.gradle.enterprise").version("3.8.1")
-    id("com.gradle.enterprise.gradle-enterprise-conventions-plugin").version("0.7.6")
+    id("io.github.gradle.gradle-enterprise-conventions-plugin").version("0.7.6")
 }
 
 dependencyResolutionManagement {

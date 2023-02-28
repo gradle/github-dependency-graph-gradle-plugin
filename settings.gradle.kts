1ed99b1b5fdb22a5d4f358cf75898890b6c358d7
Daz DeBoer <daz@gradle.com>
2023-02-28 15:43:23 -0700

    Update to Gradle 7.6.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.internal.FeaturePreviews
 
 pluginManagement {
     plugins {
-        id("org.jetbrains.kotlin.jvm") version "1.5.32"
+        id("org.jetbrains.kotlin.jvm") version "1.7.10"
         id("com.github.johnrengelman.shadow") version "7.1.0"
     }
 }

533c471dfa32b38685033fd17d5f8aa7b44af13a
daz <daz@gradle.com>
2023-03-09 11:35:41 -0700

    Test transitive project dependencies

--- plugin-test/src/test/groovy/org/gradle/github/dependency/extractor/MultiProjectDependencyExtractorTest.groovy
+++ plugin-test/src/test/groovy/org/gradle/github/dependency/extractor/MultiProjectDependencyExtractorTest.groovy
@@ -152,9 +152,9 @@ class MultiProjectDependencyExtractorTest extends BaseExtractorTest {
         ":b:validate"  | false
     }
 
-    def "multi-project build where one project depends upon another when compiling"(String taskInvocation) {
+    def "multi-project build with transitive project dependencies"(String taskInvocation) {
         given:
-        multiProjectBuild("parent", ["a", "b"]) {
+        multiProjectBuild("parent", ["a", "b", "c"]) {
             List<BuildTestFile> projects = []
             projects.add project("a").tap {
                 buildFile """
@@ -166,9 +166,17 @@ class MultiProjectDependencyExtractorTest extends BaseExtractorTest {
             }
             projects.add project("b").tap {
                 buildFile """
+                apply plugin: 'java-library'
+                dependencies {
+                    api project(':a')
+                }
+                """
+            }
+            projects.add project("c").tap {
+                buildFile """
                 apply plugin: 'java'
                 dependencies {
-                    implementation project(':a')
+                    implementation project(':b')
                 }
                 """
             }
@@ -180,41 +188,57 @@ class MultiProjectDependencyExtractorTest extends BaseExtractorTest {
         succeeds(taskInvocation)
 
         then:
+        def aTestProjectPurl = "pkg:maven/org.test/a@1.0"
+        def bTestProjectPurl = "pkg:maven/org.test/b@1.0"
+
         def aCompileClasspath = jsonRepositorySnapshot(project: ":a", configuration: "compileClasspath")
         def aCompileClasspathFile = aCompileClasspath.file as Map
         aCompileClasspathFile.source_location == "a/build.gradle"
         def aClasspathResolved = aCompileClasspath.resolved as Map
-        def aTestFoo = aClasspathResolved[fooPurl] as Map
-        verifyAll(aTestFoo) {
+        verifyAll(aClasspathResolved[fooPurl] as Map) {
             purl == this.fooPurl
             relationship == "direct"
             dependencies == []
         }
+
         def bCompileClasspath = jsonRepositorySnapshot(project: ":b", configuration: "compileClasspath")
         def bCompileClasspathFile = bCompileClasspath.file as Map
         bCompileClasspathFile.source_location == "b/build.gradle"
         def bClasspathResolved = bCompileClasspath.resolved as Map
-        def bTestFoo = bClasspathResolved[fooPurl] as Map
-        verifyAll(bTestFoo) {
+        verifyAll(bClasspathResolved[fooPurl] as Map) {
             purl == this.fooPurl
             relationship == "indirect"
             dependencies == []
         }
-        def aTestProjectPurl = "pkg:maven/org.test/a@1.0"
-        def aTestProject = bClasspathResolved[aTestProjectPurl] as Map
-        verifyAll(aTestProject) {
+        verifyAll(bClasspathResolved[aTestProjectPurl] as Map) {
             purl == aTestProjectPurl
             relationship == "direct"
             dependencies == [this.fooPurl]
         }
+
+        def cCompileClasspath = jsonRepositorySnapshot(project: ":c", configuration: "compileClasspath")
+        def cCompileClasspathFile = cCompileClasspath.file as Map
+        cCompileClasspathFile.source_location == "c/build.gradle"
+        def cClasspathResolved = cCompileClasspath.resolved as Map
+        verifyAll(cClasspathResolved[fooPurl] as Map) {
+            purl == this.fooPurl
+            relationship == "indirect"
+            dependencies == []
+        }
+        verifyAll(cClasspathResolved[aTestProjectPurl] as Map) {
+            purl == aTestProjectPurl
+            relationship == "indirect"
+            dependencies == [this.fooPurl]
+        }
+        verifyAll(cClasspathResolved[bTestProjectPurl] as Map) {
+            purl == bTestProjectPurl
+            relationship == "direct"
+            dependencies == [aTestProjectPurl]
+        }
         where:
-        // Running just the 'validate' task on 'b' will not implicitly cause 'a' to be resolved.
-        // This is because the configuration 'runtimeClasspath' on 'b' relies upon the 'runtimeElements' of 'a',
-        // not the 'runtimeClasspath'. The 'runtimeElements' configuration is not itself resolvable.
-        // https://docs.gradle.org/current/userguide/java_library_plugin.html#sec:java_library_configurations_graph
         taskInvocation | _
         "classes"      | _
-        ":b:classes"   | _
+        ":c:classes"   | _
     }
 
     def "project with buildSrc"() {

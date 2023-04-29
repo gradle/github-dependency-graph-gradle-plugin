b0e65eb6ad1934aa90df23c0409e3052403fb6c5
daz <daz@gradle.com>
2023-04-29 11:36:51 -0600

    Remove dependencyResolutionManagement
    This is not required.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,36 +3,6 @@ plugins {
     id("com.gradle.common-custom-user-data-gradle-plugin").version("1.8.2")
 }
 
-dependencyResolutionManagement {
-    repositories {
-        maven {
-            name = "Gradle public repository"
-            url = uri("https://repo.gradle.org/gradle/public")
-            content {
-                includeGroup("net.rubygrapefruit")
-                includeModule("classycle", "classycle")
-                includeModule("flot", "flot")
-                includeModule("org.gradle", "gradle-tooling-api")
-                includeModuleByRegex("org.gradle", "docs-asciidoctor-extensions(-base)?")
-            }
-        }
-        google {
-            content {
-                includeGroup("com.android.databinding")
-                includeGroupByRegex("com\\.android\\.tools(\\.[a-z.\\-]*)?")
-            }
-        }
-        maven {
-            name = "CHAMP libs"
-            url = uri("https://releases.usethesource.io/maven/")
-            mavenContent {
-                includeGroup("io.usethesource")
-            }
-        }
-        mavenCentral()
-    }
-}
-
 gradleEnterprise {
     server = "https://ge.gradle.org"
     buildScan {

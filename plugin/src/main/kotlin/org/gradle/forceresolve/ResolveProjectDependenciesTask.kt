0fabed67eccdbc84eccb47dbd315a185d4fcbb9d
daz <daz@gradle.com>
2024-04-01 14:07:40 -0600

    Apply the configuration filter for 'resolve-all-dependencies'
    Previously, excluded configurations would be excluded from the graph, but they
    would still be resolved. By avoiding resolution altogether, we allow users to
    exclude any unresolvable dependency configurations.
    
    Fixes #126

--- plugin/src/main/kotlin/org/gradle/forceresolve/ResolveProjectDependenciesTask.kt
+++ plugin/src/main/kotlin/org/gradle/forceresolve/ResolveProjectDependenciesTask.kt
@@ -4,13 +4,18 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.result.ResolvedComponentResult
 import org.gradle.api.provider.Provider
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
+import org.gradle.dependencygraph.extractor.ResolvedConfigurationFilter
 import org.gradle.internal.serialization.Cached
 import org.gradle.work.DisableCachingByDefault
 
 @DisableCachingByDefault(because = "Not worth caching")
 abstract class ResolveProjectDependenciesTask: DefaultTask() {
     private val configurationResolvers = Cached.of { createConfigurationResolvers() }
+
+    @Internal
+    var configurationFilter: ResolvedConfigurationFilter? = null
 
     private fun createConfigurationResolvers(): List<Provider<ResolvedComponentResult>> {
         return getReportableConfigurations().map {
@@ -19,7 +24,9 @@ abstract class ResolveProjectDependenciesTask: DefaultTask() {
     }
 
     private fun getReportableConfigurations(): List<Configuration> {
-        return project.configurations.filter { it.isCanBeResolved }
+        return project.configurations.filter {
+            it.isCanBeResolved && configurationFilter!!.include(project.path, it.name)
+        }
     }
 
     @TaskAction
